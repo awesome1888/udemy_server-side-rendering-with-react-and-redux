4b0d85daf615ff1e8e5c0b19e00738d8a77396b0
Sergei <gannochenko.sv@gmail.com>
2018-06-18 20:35:59 +0200

    stuck

--- server/src/index.js
+++ server/src/index.js
@@ -9,24 +9,30 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
+
+    console.dir(`Path: ${req.path}`);
+
     const store = createStore();
 
     // SERVER-SIDE RENDERING STARTS HERE
-    // some logic to initialize and load data into the store
 
     // matchRouts will look for the route and return a pack of components which are about to be rendered
-    // const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-    //     return (route.loadData ? route.loadData(store) : null);
-    // });
-    //
-    // console.dir(promises.length);
-    
-    // Promise.all(promises).then(() => {
-    //    
-    //    
-    // });
+    const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+        return (route.loadData ? route.loadData(store) : null);
+    });
 
-    res.send(renderer(req, store));
+    if (promises.length)
+    {
+        Promise.all(promises).then(() => {
+            res.send(renderer(req, store));
+        });
+    }
+    else
+    {
+        res.send(renderer(req, store));
+    }
+
+    // res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
